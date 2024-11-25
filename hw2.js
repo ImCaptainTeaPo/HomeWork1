c10ddf32d931930628baf8093337892f3fe6d600
ImCaptainTeaPo <SSS1999123@gmail.com>
2024-11-25 16:51:07 +0300

    jsExercise7

--- hw2.js
+++ hw2.js
@@ -38,9 +38,20 @@
 
 
 // ex6
-let a = 9;
-let b = 2;
+// let a = 9;
+// let b = 2;
 
-let remainder = a % b;
+// let remainder = a % b;
 
-alert(remainder);
+// alert(remainder);
+
+
+// ex7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
