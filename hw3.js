1374b77649691e687d749d1e527030bca8e63554
ImCaptainTeaPo <SSS1999123@gmail.com>
2024-12-01 15:35:30 +0300

    js ex2

--- hw3.js
+++ hw3.js
@@ -14,7 +14,13 @@ if (userInput === password) {
 
 
 // ex2
+let c = 2; // Здесь можно менять значение для проверки
 
+if (c > 0 && c < 10) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
 
 // ex3
 
