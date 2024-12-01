c44c3aa7f0a72d7e7b9985abc63a6b6921b6b671
ImCaptainTeaPo <SSS1999123@gmail.com>
2024-12-01 15:35:11 +0300

    js ex1

---
+++ hw3.js
@@ -0,0 +1,27 @@
+// ex1
+let password = 'пароль';
+
+let userInput = prompt('Введите пароль');
+
+password = password.toLowerCase();
+userInput = userInput.toLowerCase();
+
+if (userInput === password) {
+    console.log('Пароль введен верно');
+} else {
+    console.log('Пароль введен неправильно');
+}
+
+
+// ex2
+
+
+// ex3
+
+
+
+// ex4
+
+
+
+// ex5
